849af44f4513efaa6c87888032cce62033b9af06
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-11 22:23:39 +0600

    facade test added

---
+++ tests/Integration/FacadeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Anik\Testbench\Tests\Integration;
+
+use Anik\Testbench\TestCase;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
+use RuntimeException;
+
+class FacadeTest extends TestCase
+{
+    protected static $LOAD_FACADE = false;
+
+    protected function withFacade(): bool
+    {
+        // value gets changed in the following methods
+        return static::$LOAD_FACADE;
+    }
+
+    public function testFacadeIsNotLoaded()
+    {
+        // For the following test, it'll load facade.
+        static::$LOAD_FACADE = true;
+
+        $this->expectException(RuntimeException::class);
+        App::environment();
+    }
+
+    public function testFacadeIsLoaded()
+    {
+        $this->assertTrue('testing' === App::environment());
+
+        // reverting to original state
+        static::$LOAD_FACADE = false;
+    }
+
+    public function testFacadeCanBeLoadedUsingAppInstance()
+    {
+        $this->app->withFacades();
+        $this->assertTrue('testing' === App::environment());
+    }
+}
