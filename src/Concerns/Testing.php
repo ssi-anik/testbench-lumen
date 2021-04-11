9e0276d9d3c8c6c8b0d3cf0b60c3eb615979f60c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-11 14:50:24 +0600

    testing bootstrap methods added

---
+++ src/Concerns/Testing.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anik\Testbench\Concerns;
+
+use Illuminate\Support\Facades\Facade;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Lumen\Testing\WithoutEvents;
+use Laravel\Lumen\Testing\WithoutMiddleware;
+use Mockery;
+
+trait Testing
+{
+    protected $afterApplicationCreatedCallbacks = [];
+    protected $afterApplicationRefreshedCallbacks = [];
+    protected $beforeApplicationDestroyedCallbacks = [];
+    protected $afterApplicationDestroyedCallbacks = [];
+    protected $hasSetUpRun = false;
+
+    protected function fireCallbacks($callbacks, ...$parameters): void
+    {
+        foreach ($callbacks as $callback) {
+            call_user_func_array($callback, $parameters);
+        }
+    }
+
+    protected function fireAfterApplicationCreated(): void
+    {
+        $this->fireCallbacks($this->afterApplicationCreatedCallbacks, $this->app);
+    }
+
+    protected function fireAfterApplicationRefreshed(): void
+    {
+        $this->fireCallbacks($this->afterApplicationRefreshedCallbacks, $this->app);
+    }
+
+    protected function fireBeforeApplicationDestroyed(): void
+    {
+        $this->fireCallbacks($this->beforeApplicationDestroyedCallbacks, $this->app);
+    }
+
+    protected function fireAfterApplicationDestroyed(): void
+    {
+        $this->fireCallbacks($this->afterApplicationDestroyedCallbacks);
+    }
+
+    public function afterApplicationCreated(callable $callback): void
+    {
+        $this->afterApplicationCreatedCallbacks[] = $callback;
+
+        if ($this->hasSetUpRun) {
+            call_user_func_array($callback, [$this->app]);
+        }
+    }
+
+    public function afterApplicationRefreshed(callable $callback): void
+    {
+        $this->afterApplicationRefreshedCallbacks[] = $callback;
+
+        if ($this->hasSetUpRun) {
+            call_user_func_array($callback, [$this->app]);
+        }
+    }
+
+    public function beforeApplicationDestroyed(callable $callback): void
+    {
+        $this->beforeApplicationDestroyedCallbacks[] = $callback;
+    }
+
+    public function afterApplicationDestroyed(callable $callback): void
+    {
+        $this->afterApplicationDestroyedCallbacks[] = $callback;
+    }
+
+    final public function setUpTestEnvironment(): void
+    {
+        if (!$this->app) {
+            $this->refreshApplication();
+        }
+
+        $this->fireAfterApplicationRefreshed();
+
+        $this->setUpTraits();
+
+        $this->fireAfterApplicationCreated();
+
+        $this->hasSetUpRun = true;
+    }
+
+    final public function tearDownTestEnvironment(): void
+    {
+        if ($this->app) {
+            $this->fireBeforeApplicationDestroyed();
+
+            $this->app->flush();
+            $this->app = null;
+        }
+
+        $this->hasSetUpRun = false;
+
+        if (class_exists(Mockery::class)) {
+            if (($container = Mockery::getContainer()) !== null) {
+                $this->addToAssertionCount($container->mockery_getExpectationCount());
+            }
+
+            Mockery::close();
+        }
+
+        $this->afterApplicationCreatedCallbacks = [];
+        $this->afterApplicationRefreshedCallbacks = [];
+        $this->beforeApplicationDestroyedCallbacks = [];
+
+        $this->fireAfterApplicationDestroyed();
+
+        $this->afterApplicationDestroyedCallbacks = [];
+    }
+
+    protected function refreshApplication(): void
+    {
+        if ($this->withFacade()) {
+            Facade::clearResolvedInstances();
+        }
+
+        $this->app = $this->createApplication();
+
+        $url = $this->app->make('config')->get('app.url', 'http://localhost');
+
+        $this->app->make('url')->forceRootUrl($url);
+
+        $this->app->boot();
+    }
+
+    protected function reloadApplication(): void
+    {
+        $this->tearDownTestEnvironment();
+        $this->setUpTestEnvironment();
+    }
+
+    protected function setUpTraits(): void
+    {
+        $uses = array_flip(class_uses_recursive(get_class($this)));
+
+        if (isset($uses[DatabaseMigrations::class])) {
+            $this->runDatabaseMigrations();
+        }
+
+        if (isset($uses[DatabaseTransactions::class])) {
+            $this->beginDatabaseTransaction();
+        }
+
+        if (isset($uses[WithoutMiddleware::class])) {
+            $this->disableMiddlewareForAllTests();
+        }
+
+        if (isset($uses[WithoutEvents::class])) {
+            $this->disableEventsForAllTests();
+        }
+    }
+}
---
+++ src/TestCase.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anik\Testbench;
+
+use Anik\Testbench\Concerns\Auth;
+use Anik\Testbench\Concerns\Console;
+use Anik\Testbench\Concerns\CreateApplication;
+use Anik\Testbench\Concerns\Database;
+use Anik\Testbench\Concerns\Event;
+use Anik\Testbench\Concerns\Testing;
+use Laravel\Lumen\Testing\Concerns\MakesHttpRequests;
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+abstract class TestCase extends PHPUnit
+{
+    use Auth, Console, CreateApplication, Database, Event, MakesHttpRequests, Testing;
+
+    /**
+     * The application instance.
+     *
+     * @var \Laravel\Lumen\Application
+     */
+    protected $app;
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->setUpTestEnvironment();
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        $this->tearDownTestEnvironment();
+    }
+}
