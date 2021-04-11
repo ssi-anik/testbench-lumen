5eeaf355134e718de9810a46121a211416e6b47f
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-11 14:49:36 +0600

    Event & Job related methods added

---
+++ src/Concerns/Event.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anik\Testbench\Concerns;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Mockery;
+
+trait Event
+{
+    /**
+     * Specify a list of events that should be fired for the given operation.
+     *
+     * These events will be mocked, so that handlers will not actually be executed.
+     *
+     * @param array|string $events
+     *
+     * @return $this
+     */
+    public function expectsEvents($events)
+    {
+        $events = is_array($events) ? $events : func_get_args();
+
+        $mock = Mockery::spy(Dispatcher::class);
+
+        $mock->shouldReceive('dispatch')->andReturnUsing(function ($called) use (&$events) {
+            foreach ($events as $key => $event) {
+                if ((is_string($called) && $called === $event) || (is_string($called) && is_subclass_of($called,
+                            $event)) || (is_object($called) && $called instanceof $event)) {
+                    unset($events[$key]);
+                }
+            }
+        });
+
+        $this->beforeApplicationDestroyed(function () use (&$events) {
+            if ($events) {
+                throw new Exception('The following events were not fired: [' . implode(', ', $events) . ']');
+            }
+        });
+
+        $this->app->instance('events', $mock);
+
+        return $this;
+    }
+
+    /**
+     * Mock the event dispatcher so all events are silenced.
+     *
+     * @return $this
+     */
+    protected function withoutEvents()
+    {
+        $mock = Mockery::mock(Dispatcher::class);
+
+        $mock->shouldReceive('dispatch');
+
+        $this->app->instance('events', $mock);
+
+        return $this;
+    }
+}
---
+++ src/Concerns/Job.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anik\Testbench\Concerns;
+
+use Mockery;
+
+trait Job
+{
+    /**
+     * Specify a list of jobs that should be dispatched for the given operation.
+     *
+     * These jobs will be mocked, so that handlers will not actually be executed.
+     *
+     * @param array|string $jobs
+     *
+     * @return $this
+     */
+    protected function expectsJobs($jobs)
+    {
+        $jobs = is_array($jobs) ? $jobs : func_get_args();
+
+        $mock = Mockery::mock('Illuminate\Bus\Dispatcher[dispatch]', [$this->app]);
+
+        foreach ($jobs as $job) {
+            $mock->shouldReceive('dispatch')->atLeast()->once()->with(Mockery::type($job));
+        }
+
+        $this->app->instance(\Illuminate\Contracts\Bus\Dispatcher::class, $mock);
+
+        return $this;
+    }
+
+    /**
+     * Mock the job dispatcher so all jobs are silenced and collected.
+     *
+     * @return $this
+     */
+    protected function withoutJobs()
+    {
+        $mock = Mockery::mock('Illuminate\Bus\Dispatcher[dispatch]', [$this->app]);
+
+        $mock->shouldReceive('dispatch')->andReturnUsing(function ($dispatched) {
+            $this->dispatchedJobs[] = $dispatched;
+        });
+
+        $this->app->instance(\Illuminate\Contracts\Bus\Dispatcher::class, $mock);
+
+        return $this;
+    }
+}
