8037f4f6564d9c8fee0b747c7c539f79dea840bc
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-11 23:46:23 +0600

    route binding test added

---
+++ tests/Integration/RouteBindingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Anik\Testbench\Tests\Integration;
+
+use Anik\Testbench\TestCase;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Router;
+
+class RouteBindingTest extends TestCase
+{
+    protected function routes(Router $router): void
+    {
+        $router->get('/test', function (Request $request) {
+            return response()->json([
+                'error' => false,
+                'message' => 'I can be reached',
+                'data' => $request->all(),
+            ], 202);
+        });
+    }
+
+    public function testDefaultRouteShouldBeAvailable()
+    {
+        $response = $this->get('/')->seeStatusCode(200)->response->getContent();
+        $this->assertTrue($response == $this->app->version());
+    }
+
+    public function testRouteCanBeBoundSuccessfully()
+    {
+        $this->json('GET', 'test')->seeStatusCode(202);
+    }
+
+    public function testDependencyInjectionIsWorkingCorrectly()
+    {
+        $this->call('GET', 'test', ['name' => 'testbench-lumen', 'env' => 'testing']);
+        $response = $this->response->getData(true);
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertArrayHasKey('name', $response['data']);
+        $this->assertArrayHasKey('env', $response['data']);
+        $this->assertTrue('testing' == $response['data']['env']);
+        $this->assertTrue('testbench-lumen' == $response['data']['name']);
+    }
+}
