2a11bb87d91dca1cb6669db348d2326ad8b1ac75
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-12 06:44:25 +0600

    events test added

---
+++ tests/Integration/EventTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Anik\Testbench\Tests\Integration;
+
+use Anik\Testbench\TestCase;
+use App\Events\ExampleEvent;
+use App\Providers\EventServiceProvider;
+
+class EventTest extends TestCase
+{
+    protected function serviceProviders(): array
+    {
+        return [EventServiceProvider::class];
+    }
+
+    public function testFiredEventsCanBeListened()
+    {
+        $this->expectsEvents(ExampleEvent::class);
+        $this->app['events']->dispatch(new ExampleEvent());
+    }
+}
