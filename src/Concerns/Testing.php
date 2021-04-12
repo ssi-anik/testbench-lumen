b16c658ba41fddebdd882299921bd7e81666d51a
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-12 18:53:06 +0600

    psr12 line length warning fix

--- src/Concerns/Testing.php
+++ src/Concerns/Testing.php
@@ -161,8 +161,9 @@ trait Testing
 
     final protected function runThroughAnnotatedMethods()
     {
-        array_reduce($this->parseAnnotation('setup-before', static::class, $this->getName(false)), function ($carry, $next) {
-            return call_user_func_array([$this, $next], [$this->app, $carry]);
-        });
+        array_reduce($this->parseAnnotation('setup-before', static::class, $this->getName(false)),
+            function ($carry, $next) {
+                return call_user_func_array([$this, $next], [$this->app, $carry]);
+            });
     }
 }
