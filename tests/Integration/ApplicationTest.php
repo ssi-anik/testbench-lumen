aa16a58a32885452731ad6bc18fce2e14989fbd6
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-12 08:05:24 +0600

    ApplicationTest updated with app reload method checking

--- tests/Integration/ApplicationTest.php
+++ tests/Integration/ApplicationTest.php
@@ -3,6 +3,7 @@
 namespace Anik\Testbench\Tests\Integration;
 
 use Anik\Testbench\TestCase;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Laravel\Lumen\Application;
 
 class ApplicationTest extends TestCase
@@ -10,5 +11,17 @@ class ApplicationTest extends TestCase
     public function testAppPropertyInstanceOfLumenApplication()
     {
         $this->assertTrue($this->app instanceof Application);
+    }
+
+    public function testReloadApplication()
+    {
+        $this->app->bind('should-exists', function () {
+            return 'does-exist';
+        });
+        $this->assertTrue($this->app->make('should-exists') == 'does-exist');
+
+        $this->reloadApplication();
+        $this->expectException(BindingResolutionException::class);
+        $this->app->make('should-exist');
     }
 }
