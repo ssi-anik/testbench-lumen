2206542d77c299b8e5b3d3dcf47a2d6ce6a325de
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-12 08:20:07 +0600

    middleware trait test added

---
+++ tests/Integration/TraitTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Anik\Testbench\Tests\Integration;
+
+use Anik\Testbench\TestCase;
+use App\Providers\AuthServiceProvider;
+use Laravel\Lumen\Routing\Router;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Lumen\Testing\WithoutEvents;
+use Laravel\Lumen\Testing\WithoutMiddleware;
+
+class TraitTest extends TestCase
+{
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+    use WithoutMiddleware;
+    use WithoutEvents;
+
+    protected function serviceProviders(): array
+    {
+        return [AuthServiceProvider::class];
+    }
+
+    protected function withEloquent(): bool
+    {
+        return true;
+    }
+
+    protected function routes(Router $router): void
+    {
+        $router->group(['middleware' => 'auth'], function ($router) {
+            $router->get('guarded', function () {
+                return response()->json(['user' => auth()->user() ?? 'middleware bypassed']);
+            });
+        });
+    }
+
+    public function testWithoutMiddleware()
+    {
+        $response = json_decode($this->get('/guarded')->seeStatusCode(200)->response->getContent(), true);
+        $this->assertTrue($response['user'] == 'middleware bypassed');
+    }
+}
