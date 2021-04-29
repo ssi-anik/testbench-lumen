06c87a90578fea8e0c300be6434be5b5afb5b67d
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-04-30 04:17:24 +0600

    artisan methods added as helpers

--- src/Concerns/Console.php
+++ src/Concerns/Console.php
@@ -11,13 +11,37 @@ trait Console
     /**
      * Call artisan command and return code.
      *
-     * @param string $command
+     * @param string|null $command
      * @param array $parameters
      *
-     * @return int
+     * @return int|Kernel
      */
-    public function artisan(string $command, $parameters = []): int
+    public function artisan(?string $command = null, $parameters = [])
     {
+        if (is_null($command)) {
+            return $this->console();
+        }
+
         return $this->app[Kernel::class]->call($command, $parameters);
+    }
+
+    /**
+     * Get the latest artisan console output
+     *
+     * @return string
+     */
+    public function artisanOutput(): string
+    {
+        return $this->app[Kernel::class]->output();
+    }
+
+    /**
+     * Get the Artisan Kernel instance
+     *
+     * @return Kernel
+     */
+    public function console(): Kernel
+    {
+        return $this->app[Kernel::class];
     }
 }
--- tests/Integration/ArtisanTest.php
+++ tests/Integration/ArtisanTest.php
@@ -3,12 +3,28 @@
 namespace Anik\Testbench\Tests\Integration;
 
 use Anik\Testbench\TestCase;
+use App\Console\Kernel;
 
 class ArtisanTest extends TestCase
 {
     public function testArtisanCallRunsOkay()
     {
+        $this->assertTrue($this->artisan() instanceof Kernel);
         $this->assertTrue(0 === $this->artisan('help'));
         $this->assertTrue(0 === $this->artisan('list'));
+    }
+
+    public function testArtisanOutput()
+    {
+        $this->artisan('cache:clear');
+        $this->assertStringContainsString('Application cache cleared', $this->artisanOutput());
+    }
+
+    public function testConsoleInstance()
+    {
+        $this->assertTrue($this->console() instanceof Kernel);
+        $returned = $this->console()->call('cache:clear');
+        $this->assertEquals(0, $returned);
+        $this->assertStringContainsString('Application cache cleared', $this->console()->output());
     }
 }
